ba3217fffafbb7d703082be65aac59b7098c7210
Martha Alves <106412221+Martha-Alves@users.noreply.github.com>
2023-03-03 17:18:26 -0300

    mexendo na regex

--- verificador.js
+++ verificador.js
@@ -1,31 +1,31 @@
 function palindrome(str) {
 
-   let reversedStr = ""
-    
-    
-
-    for (let i = str.length - 1; i >= 0; i--) {
-      reversedStr += str[i];
-    }
-
-    str = str
-    .split(" ")
-    .join("")
-    .toLowerCase()
-
-    let myStr = reversedStr
-      .split(" ")
-      .join("")
-      .toLowerCase()
-    
-    if (myStr === str) {
-      return true
-    } else {
-      return false
-    }
-
-    
-  // return str
-}
+    let reversedStr = ""
+     
+     
+ 
+     for (let i = str.length - 1; i >= 0; i--) {
+       reversedStr += str[i];
+     }
+ 
+     str = str
+     .split(/\W*/)
+     .join("")
+     .toLowerCase()
+ 
+     let myStr = reversedStr
+       .split(/\W*/)
+       .join("")
+       .toLowerCase()
+     
+     if (myStr === str) {
+       return true
+     } else {
+       return false
+     }
+ 
+     
+   // return str
+ };
 
 console.log(palindrome("race CAR"));
