91a56924b9e876444102fded89300e9efa2d6326
Martha Alves <106412221+Martha-Alves@users.noreply.github.com>
2023-03-03 17:29:10 -0300

    subindo versão final

--- verificador.js
+++ verificador.js
@@ -1,31 +1,33 @@
 function palindrome(str) {
 
     let reversedStr = ""
-     
-     
- 
-     for (let i = str.length - 1; i >= 0; i--) {
-       reversedStr += str[i];
-     }
- 
-     str = str
-     .split(/\W*/)
-     .join("")
-     .toLowerCase()
- 
-     let myStr = reversedStr
-       .split(/\W*/)
-       .join("")
-       .toLowerCase()
-     
-     if (myStr === str) {
-       return true
-     } else {
-       return false
-     }
- 
-     
-   // return str
- };
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedStr += str[i];
+    }
+
+    str = str
+        .split(/\W*/)
+        .join("")
+        .replace('_', '')
+        .toLowerCase()
+
+    let myStr = reversedStr
+        .split(/\W*/)
+        .join("")
+        .replace('_', '')
+        .toLowerCase()
+
+    if (myStr === str) {
+        return true
+    } else {
+        return false
+    }
+
+}
+
+
+
+
 
 console.log(palindrome("race CAR"));
